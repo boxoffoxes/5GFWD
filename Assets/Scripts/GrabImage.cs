713ec05106d6d9f4ac909b8e84f9e337f0bba375
Glyn <void@killmenow.co.uk>
2017-06-10 17:28:28 +0300

    First attempt at texture setting by rpc

--- Assets/Scripts/GrabImage.cs
+++ Assets/Scripts/GrabImage.cs
@@ -4,26 +4,34 @@ using UnityEngine.Networking;
 using UnityEngine;
 
 public class GrabImage : NetworkBehaviour {
+    public GameObject plane;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
         RenderTexture tx = this.GetComponent<Camera>().targetTexture;
         RenderTexture.active = tx;
         Texture2D t2 = new Texture2D(tx.width, tx.height);
-        t2.ReadPixels(new Rect(0, 0, tx.width, tx.height),0,0);
-        RenderTexture.active = null;
+        t2.ReadPixels(new Rect(0, 0, tx.width, tx.height), 0, 0);
         byte[] png = t2.EncodeToPNG();
         System.IO.File.WriteAllBytes("blah.png", png);
-    	RpcDebugMessages();
+        if (isServer) {
+            RpcDebugMessages(png);
+        } else {
+
+        }
+        RenderTexture.active = null;
     }
     [ClientRpc]
-    void RpcDebugMessages()
+    void RpcDebugMessages(byte[] png)
     {
         Debug.Log("I'm so happy");
+        Texture2D t = new Texture2D(256,256);
+        t.LoadImage(png);
+        plane.transform.GetComponent<Material>().SetTexture("_MainTex", t);
     }
 }
